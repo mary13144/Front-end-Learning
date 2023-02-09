019b40e93dc8a973ef381797c5c7c4589a878bea
muwenxin <1427183122@qq.com>
2023-02-10 00:21:10 +0800

    day02

--- LeetCode/剑指offer/day02/剑指Offer35_复杂链表的复制.js
+++ LeetCode/剑指offer/day02/剑指Offer35_复杂链表的复制.js
@@ -11,6 +11,31 @@
  * @param {Node} head
  * @return {Node}
  */
+// var copyRandomList = function (head) {
+// 	if (head === null) {
+// 		return null;
+// 	}
+// 	let newhead = new Node();
+// 	let cur = head;
+// 	let newcur = newhead;
+// 	let map = new Map();
+// 	while (cur) {
+// 		newcur.val = cur.val;
+// 		newcur.next = cur.next ? new Node() : null;
+// 		map.set(cur, newcur);
+// 		cur = cur.next;
+// 		newcur = newcur.next;
+// 	}
+// 	result = newhead;
+// 	while (head) {
+// 		newhead.random = head.random ? map.get(head.random) : null;
+// 		head = head.next;
+// 		newhead = newhead.next;
+// 	}
+//
+// 	return result;
+// };
+
 var copyRandomList = function (head) {
 	if (head === null) {
 		return null;
@@ -18,17 +43,16 @@ var copyRandomList = function (head) {
 	let newhead = new Node();
 	let cur = head;
 	let newcur = newhead;
-	let map = new Map();
 	while (cur) {
 		newcur.val = cur.val;
 		newcur.next = cur.next ? new Node() : null;
-		map.set(cur, newcur);
+		cur.val = newcur;
 		cur = cur.next;
 		newcur = newcur.next;
 	}
 	result = newhead;
 	while (head) {
-		newhead.random = head.random ? map.get(head.random) : null;
+		newhead.random = head.random ? head.random.val : null;
 		head = head.next;
 		newhead = newhead.next;
 	}
